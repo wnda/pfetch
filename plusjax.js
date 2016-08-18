6e91c5b25eee944ead5a6aa5ca92adbd2e5c770f
A. M. Douglas <me@amdouglas.com>
2016-08-18 04:08:46 +0100

    Update plusjax.js

--- plusjax.js
+++ plusjax.js
@@ -2,21 +2,15 @@
 
   var container = 'main';
 
-
   var find = function( selector, context ){
-
     return (context || document).querySelector( selector );
-
   };
 
   var stripHash = function( location ){
-
     return location.href.replace(/#.*/, '');
-
   };
 
   var getAnchor = function( element ){
-
     if ( element.tagName.toLowerCase() === 'body' )
     {
       return false;
@@ -25,6 +19,7 @@
     if ( !!element.href && element.tagName.toLowerCase() === 'a' )
     {
       asyncLoad( element );
+      return false;
     }
 
     getAnchor( element.parentNode );
@@ -49,81 +44,100 @@
     }
 
     loadPage( link.href );
-
     history.pushState( null, null, link.href );
 
   };
 
   var loadPage = function( url ){
-
-    var done = false;
     
     var hash = !!( url.indexOf( '#' ) > -1 );
-
-    var xhr = new XMLHttpRequest();
-
-    xhr.open( 'GET', url );
-
-    xhr.responseType = 'document';
     
-    // if document not available
-    // or using fetch
-    // var parser = new DOMParser();
-    // var doc = parser.parseFromString(this.response, "text/html");
-    // var newPage = find( container, doc );
-
-    xhr.onreadystatechange = function (){
-
-      if ( this.readyState === 4 && this.status >= 200 && this.status < 300 )
+    if ( "fetch" in window )
+    {
+      fetch( url )
+      .then(function(resp)
       {
-        done = true;
-
-        var newPage = find( container, this.response );
-
-        var newTitle = find( 'title', this.response ).textContent;
-
-        var currentPage = find( container );
-
-        find( 'title' ).textContent = newTitle;
-
-        currentPage.parentNode.replaceChild( newPage, currentPage );
-        
-        if(!hash)
+        if ( resp.ok || resp.status >= 200 && resp.status < 300 )
         {
-          window.scrollTo(0, 0);
+          return resp.text().then(function(txt){
+            var parser = new DOMParser(),
+                doc = parser.parseFromString( txt, "text/html" ),
+                newPage = find( container, doc ),
+                newTitle = find( 'title', doc ).textContent,
+                currentPage = find( container );
+          
+            find( 'title' ).textContent = newTitle;
+            currentPage.parentNode.replaceChild( newPage, currentPage );
+            
+            if( !hash )
+            {
+              window.scrollTo(0, 0);
+            }
+          })
+          .catch(function(){
+            console.log( "No markup received" )
+          });
         }
-      }
-    };
-
-    xhr.onabort = xhr.onerror = function() {
-      if ( !done )
+      })
+      .catch(function(resp)
       {
-        done = true;
-        console.info('There was an error with your request');
-      }
-    };
-
-    xhr.send();
+        console.error( 'Error: ' + ( resp.message || 'No data available' ) );
+      });
+    }
+    
+    else
+    {
+      var done = false,
+          xhr = new XMLHttpRequest();
+  
+      xhr.open( 'GET', url );
+      xhr.responseType = 'document';
+      
+      xhr.onreadystatechange = function (){
+        if ( this.readyState === 4 && this.status >= 200 && this.status < 300 )
+        {
+          done = true;
+          
+          var newPage = find( container, this.response ),
+              newTitle = find( 'title', this.response ).textContent,
+              currentPage = find( container );
+          
+          find( 'title' ).textContent = newTitle;
+          currentPage.parentNode.replaceChild( newPage, currentPage );
+          
+          if( !hash )
+          {
+            window.scrollTo(0, 0);
+          }
+          
+        }
+        
+      };
+  
+      xhr.onabort = xhr.onerror = function() {
+        if ( !done )
+        {
+          done = true;
+          console.info('There was an error with the request');
+        }
+      };
+  
+      xhr.send();
+    }
+    
   };
 
-  if (history && history.pushState)
+  if ( history && history.pushState )
   {
-    find( 'body' ).addEventListener( 'click', function( e ) {
-
+    find( 'body' ).addEventListener( 'click', function( e ){
       e.preventDefault();
-
       getAnchor( e.target );
-
     });
 
     setTimeout(function(){
-
       window.onpopstate = function(){
-
         loadPage( window.location.href );
-
       };
-
     }, 1e3);
 
   }
