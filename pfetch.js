b7c2bd16a31bbdbb1a7a10a7762d95ec0a77ef70
A. M. Douglas <me@amdouglas.com>
2016-08-18 12:18:07 +0100

    Update pfetch.js

--- pfetch.js
+++ pfetch.js
@@ -11,30 +11,25 @@
   };
 
   var getAnchor = function( element ){
-    
     if ( element.tagName.toLowerCase() === 'body' ){ return; }
     if ( !!element.href && element.tagName.toLowerCase() === 'a' ){ asyncLoad( element ); return; }
-
     getAnchor( element.parentNode );
-
   };
 
   var asyncLoad = function( link ){
-
-    if ( !link ) { return; }
+    if ( !link ){ return; }
     if ( window.location.protocol !== link.protocol || window.location.hostname !== link.hostname ){ return; }
-    if ( link.href.indexOf( '#' ) > -1 && stripHash( link ) == stripHash( window.location ) ) { return; }
-
+    if ( link.href.indexOf( '#' ) > -1 && stripHash( link ) == stripHash( window.location ) ){ return; }
+    
     loadPage( link.href );
     history.pushState( null, null, link.href );
-
   };
 
   var loadPage = function( url ){
     
     var hash = !!( url.indexOf( '#' ) > -1 );
     
-    if ( "fetch" in window ) {
+    if ( "fetch" in window ){
       
       fetch( url, { 
         method: 'GET',
@@ -43,8 +38,8 @@
       }).then( function( response ){
         
         var contentType = response.headers.get( 'Content-Type' );
-        if ( response.ok && contentType && contentType.indexOf( 'text/html' ) !== -1 )
-        {
+        
+        if ( response.ok && contentType && contentType.indexOf( 'text/html' ) !== -1 ){
           return response.text().then( function( resptxt ){
             
             var parser = new DOMParser(),
@@ -52,57 +47,50 @@
                 newPage = find( container, doc ),
                 newTitle = find( 'title', doc ).textContent,
                 currentPage = find( container );
-          
+            
             find( 'title' ).textContent = newTitle;
             currentPage.parentNode.replaceChild( newPage, currentPage );
+            
             if( !hash ) { window.scrollTo(0, 0); }
             
           }).catch( function(){ console.info( 'Error: No HTML received' ) });
         }
       }).catch( function( response ){ console.info( 'Error: ' + ( response.message || 'No data available' ) ) });
     
-    }
-    
-    else
-    {
+    } else {
+      
       var done = false,
           xhr = new XMLHttpRequest();
-  
+          
       xhr.open( 'GET', url );
       xhr.responseType = 'document';
       
       xhr.onreadystatechange = function (){
-        if ( this.readyState === 4 && this.status >= 200 && this.status < 300 )
-        {
+        if ( this.readyState === 4 && this.status >= 200 && this.status < 300 ){
           done = true;
-          
+      
           var newPage = find( container, this.response ),
               newTitle = find( 'title', this.response ).textContent,
               currentPage = find( container );
-          
+      
           find( 'title' ).textContent = newTitle;
           currentPage.parentNode.replaceChild( newPage, currentPage );
-          
+      
           if( !hash ) { window.scrollTo(0, 0); }
-          
         }
-        
       };
   
-      xhr.onabort = xhr.onerror = function() {
-        if ( !done )
-        {
-          done = true;
-          console.info( 'There was an error with the request' );
-        }
+      xhr.onabort = xhr.onerror = function(){
+        if ( !done ) { done = true;console.info( 'There was an error with the request' ); }
       };
   
       xhr.send();
+      
     }
     
   };
 
-  if ( history && history.pushState ) {
+  if ( history && history.pushState ){
     find( 'body' ).addEventListener( 'click', function( e ){
       e.preventDefault();
       getAnchor( e.target );
