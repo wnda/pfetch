5a3291334c8435aca4d38d4b131f8e390b30ec88
A. M. Douglas <me@amdouglas.com>
2016-08-28 22:06:06 +0100

    Update pfetch.js

--- pfetch.js
+++ pfetch.js
@@ -5,34 +5,29 @@
   var find = function( selector, context ){
     return ( context || document ).querySelector( selector );
   };
-  
+
   var findAll = function( selector, context ){
     return ( context || document ).querySelectorAll( selector );
   };
-  
+
   var findAllByTagName = function( selector, context ){
     return ( context || document ).getElementsByTagName( selector );
   };
-  
-  var removeScripts = function( doc ){
-    var scripts = doc.getElementsByTagName( 'script' ),
-        i = scripts.length,
-        j = 0;
-    for(; i > j; j++){
-      scripts[j].parentNode.removeChild(scripts[j]);
-    }
-    return doc;
-  };
-  
+
   var appendAndRunScripts = function( new_scripts ){
     var i = new_scripts.length, j = 0;
     for( ; i > j; j++ )
     {
       var script_to_add = document.createElement( 'script' );
-      if( new_scripts[j].type ){ script_to_add.type = new_scripts[j].type;}
-      if( new_scripts[j].async ){ script_to_add.setAttribute('async','');}
-      if( new_scripts[j].src ){ script_to_add.src = new_scripts[j].src;}
-      else{ script_to_add.textContent = new_scripts[nso].textContent;}
+
+      if( new_scripts[j].type ) script_to_add.type = new_scripts[j].type;
+
+      if( new_scripts[j].async ) script_to_add.setAttribute( 'async','' );
+
+      if( new_scripts[j].src ) script_to_add.src = new_scripts[j].src;
+
+      if( new_scripts[j].textContent ) script_to_add.textContent = new_scripts[j].textContent;
+
       ( document.head || document.getElementsByTagName('head')[0] ).appendChild( script_to_add );
     }
   };
@@ -42,77 +37,73 @@
   };
 
   var getLinkFromEventTarget = function( element ){
-    if ( element.tagName.toLowerCase() === 'body' ){ return; } 
-    if ( !!element.href && element.tagName.toLowerCase() === 'a' ){ processLink( element ); return; }
+    if ( element.tagName.toLowerCase() === 'body' ){ throw new Error( 'Reached BODY element' ); }
+    if ( !!element.href && element.tagName.toLowerCase() === 'a' ) { processLink( element ); return; }
     getLinkFromEventTarget( element.parentNode );
   };
 
   var processLink = function( link ){
-    if ( !link ){ return; }
-    if ( window.location.protocol !== link.protocol || window.location.hostname !== link.hostname ){ return; }
-    if ( link.href.indexOf( '#' ) > -1 && stripHash( link ) == stripHash( window.location ) ){ return; }
-    
+    if ( !link ) return;
+    if ( window.location.protocol !== link.protocol || window.location.hostname !== link.hostname ) return;
+    if ( link.href.indexOf( '#' ) > -1 && stripHash( link ) == stripHash( window.location ) ) return;
     loadPage( link.href );
     history.pushState( null, null, link.href );
   };
-  
-  var processNewHTML = function( ){
+
+  var processNewHTML = function( resp_txt, url ){
     var parser = new DOMParser(),
-        doc = parser.parseFromString( resptxt, 'text/html' ),
-        processed_doc = removeScripts( doc ),
-        newPage = find( container, processed_doc ),
-        newTitle = find( 'title', processed_doc ).textContent,
-        currentPage = find( container ),
-        new_scripts = findAllByTagName( 'script', doc );
-            
-    find( 'title' ).textContent = newTitle;
-    currentPage.parentNode.replaceChild( newPage, currentPage );
+        doc = parser.parseFromString( resp_txt, 'text/html' ),
+        new_page = find( container, doc ),
+        new_title = find( 'title', doc ).textContent,
+        current_page = find( container ),
+        new_scripts = findAllByTagName( 'script', doc ),
+        hash = ( url.indexOf( '#' ) > -1 );
+
+    find( 'title' ).textContent = new_title;
+    current_page.parentNode.replaceChild( new_page, current_page );
     appendAndRunScripts( new_scripts );
-    
-    if( !hash ){ window.scrollTo( 0, 0 ); }
+
+    if( !hash ) window.scrollTo( 0, 0 );
   };
 
   var loadPage = function( url ){
-    
-    var hash = !!( url.indexOf( '#' ) > -1 );
-    
+
     if ( "fetch" in window ){
-      
-      fetch( url, { 
+
+      fetch( url, {
         method: 'GET',
         headers: { 'Content-Type' : 'text/html' },
         mode: 'same-origin'
       }).then( function( response ){
         var contentType = response.headers.get( 'Content-Type' );
         if ( response.ok && contentType && contentType.indexOf( 'text/html' ) !== -1 ){
-          return response.text().then( function( resptxt ){
-            processNewHTML( resptxt );
-          }).catch( function(){ console.info( 'Error: failed to parse new HTML' ) });
+          return response.text().then( function( resp_txt ){
+            processNewHTML( resp_txt, url );
+          }).catch( function(){ console.error( 'Error: failed to parse new HTML' ); });
         }
-      }).catch( function( response ){ console.info( 'Error: ' + ( response.message || 'No data available' ) ) });
-  
+      }).catch( function( response ){ console.error( 'Error: ' + ( response.message || 'No data available' ) ); });
+
     } else {
-      
+
       var xhr = new XMLHttpRequest();
       xhr.open( 'GET', url );
       xhr.onreadystatechange = function(){
         if ( this.readyState === 4 && this.status >= 200 && this.status < 300 ){
-          processNewHTML( this.response );
+          processNewHTML( this.response, url );
         }
       };
       xhr.onabort = xhr.onerror = function(){
-        console.info( 'There was an error with the request' );
+        console.error( 'There was an error with the request' );
       };
       xhr.send();
-      
     }
   };
 
   if ( history && history.pushState ){
-    
+
     find( 'body' ).addEventListener( 'click', function( e ){
-      if ( e.which > 1 || e.metaKey || e.ctrlKey || e.shiftKey || e.altKey ){ return; }
       e.preventDefault();
+      if ( e.which > 1 || e.metaKey || e.ctrlKey || e.shiftKey || e.altKey ) return;
       getLinkFromEventTarget( e.target );
     });
 
@@ -121,6 +112,6 @@
         loadPage( window.location.href );
       };
     }, 1e3);
-    
+
   }
 })();
